d4370158362162521f5276198abceb2b95b50aab
Dhyan Patel <pateldhyanp22@gmail.com>
2026-02-11 12:41:51 +0530

    Delete learning26-master/Django/learning26 directory

--- learning26-master/Django/learning26/student/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Student,product,movie
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(product)
-admin.site.register(movie)
--- learning26-master/Django/learning26/student/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('marks/',views.marks),
-    path('info/',views.info),
-    path('teachers/',views.faculty)
-]
